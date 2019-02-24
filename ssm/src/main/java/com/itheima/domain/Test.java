b837898f9ec05b54aac8a96398235f34bda987de
lizhihe <1240201117@qq.com>
2019-02-24 22:33:51 +0800

    ssm-exam version0.2

--- ssm/src/main/java/com/itheima/domain/Test.java
+++ ssm/src/main/java/com/itheima/domain/Test.java
@@ -5,6 +5,23 @@ package com.itheima.domain;
  * @Date: 2019/2/24 22:32
  * @Version 1.0
  */
+
 public class Test {
 
+    private String testName;
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "testName='" + testName + '\'' +
+                '}';
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
 }
